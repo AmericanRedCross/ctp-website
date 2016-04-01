28bf5825cb27daf7055806952d05891a8b993fa8
danbjoseph <danbjoseph@gmail.com>
2016-04-01 15:58:10 -0400

    search on toolkit page

--- js/page.js
+++ js/page.js
@@ -2,6 +2,7 @@
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
+        console.log($($anchor.attr('href')).offset().top)
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
