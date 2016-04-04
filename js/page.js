9efefc0a9d5f07673bb16d65c448cf25719ebcf4
danbjoseph <danbjoseph@gmail.com>
2016-04-04 17:01:51 -0400

    good draft of toolkit page done

--- js/page.js
+++ js/page.js
@@ -2,7 +2,6 @@
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
-        console.log($($anchor.attr('href')).offset().top)
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
